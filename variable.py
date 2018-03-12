66cdfa9419607b8afd7093d07088847c67e6c199
sschatz451 <sebastian@pandh-entertainment.com>
2018-03-12 15:05:30 +0100

    Nochmal rechnen? HinzugefÃgt

--- variable.py
+++ variable.py
@@ -1,21 +1,34 @@
 # variable
 
-aktion =  input("Welche Aktion willst du? (+/-/*/:): ")
-print ("Aktion", aktion, "wird ausgeführt...")
+x = True
+while(x):
+	aktion =  input("Welche Aktion willst du? (+|-|*|/): ")
+	print ("Aktion", aktion, "wird ausgeführt...")
 
-eingabe1 = input("Bitte erste Zahl eingeben: ")
-print ("Zahl 1=", eingabe1)
+	eingabe1 = input("Bitte erste Zahl eingeben: ")
+	print ("Zahl 1=", eingabe1)
 
-eingabe2 = input("Bitte zweite Zahl eingeben: ")
-print ("Zahl 2=", eingabe2)
+	eingabe2 = input("Bitte zweite Zahl eingeben: ")
+	print ("Zahl 2=", eingabe2)
 
-if (aktion == "+"):
-	ausgabe = int(eingabe1) + int(eingabe2)
-if (aktion == "-"):
-	ausgabe = int(eingabe1) - int(eingabe2)
-if (aktion == "*"):
-	ausgabe = int(eingabe1) * int(eingabe2)
-if (aktion == "/"):
-	ausgabe = int(eingabe1) / int(eingabe2)
-	
-print(eingabe1, aktion, eingabe2, " = ", ausgabe)
+	if (aktion == "+"):
+		ausgabe = int(eingabe1) + int(eingabe2)
+	if (aktion == "-"):
+		ausgabe = int(eingabe1) - int(eingabe2)
+	if (aktion == "*"):
+		ausgabe = int(eingabe1) * int(eingabe2)
+	if (aktion == "/"):
+		ausgabe = int(eingabe1) / int(eingabe2)
+		
+	print(eingabe1, aktion, eingabe2, " = ", ausgabe)
+	z = True
+	while(z):
+		ende = input("\nNochmal rechnen? (Ja/Nein): ")
+		if (ende == "Ja"):
+			z = False
+			x = True
+		elif (ende == "Nein"):
+			x = False
+			z = False
+		else:
+			print("Fehler")
