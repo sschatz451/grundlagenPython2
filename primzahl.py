40461b1a0470a674ab75d2e2db284848fd8cfe8f
sschatz451 <sebastian@pandh-entertainment.com>
2018-03-12 15:57:47 +0100

    2.0

--- primzahl.py
+++ primzahl.py
@@ -1,2 +1,20 @@
 # Primzahl
 # If Eingabe ist Primzahl
+# Sebastian
+eingabe = input("Bitte Zahl eingeben: ")
+counter = 0
+x = True
+if (zahl <= 1):
+	print(zahl, "ist keine gültige eingabe.")
+else:
+	while(x):
+		if (zahl % i == 0):
+			counter = 1
+		if (i == 1):
+			x = False
+			zahl = zahl+1
+
+if (counter == 1):
+	print(zahl, "ist keine Primzahl.")
+else:
+	print(zahl, "ist eine Primzahl.")
